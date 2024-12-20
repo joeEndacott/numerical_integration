b6c18ef1f219a347f7aa36d9023bee6bcbf21bc2
Joe Endacott <joseph.endacott@gmail.com>
2024-12-20 16:53:47 +0000

    Completed the second iteration of the variable step size Simpson algorithm. I have improved the logic for changing the segment size. To do: improve the documentation, and rewrite the code using the Grid struct.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 pub mod numerical_integration_algorithms;
 pub mod test_functions;
-// pub mod variable_step_size_numerical_integration_algorithm;
+pub mod variable_step_size_numerical_integration_algorithm;
 
-use numerical_integration_algorithms::fixed_step_simpson as numeric_integral;
 use test_functions::{cubic, gaussian, quartic};
+use variable_step_size_numerical_integration_algorithm::variable_step_simpson as numeric_integral;
 
 fn main() {
     println!(
         "Integral of x^3 from 0 to 1: {}",
-        numeric_integral(cubic, 0.0, 1.0, 100)
+        numeric_integral(cubic, 0.0, 1.0, 0.1, 0.000001)
     );
     println!(
         "Integral of x^4 from 0 to 1: {}",
-        numeric_integral(quartic, 0.0, 1.0, 100)
+        numeric_integral(quartic, 0.0, 1.0, 0.1, 0.000001)
     );
     println!(
         "Integral of e^(-x^2) from -10 to 10: {}",
-        numeric_integral(gaussian, -10.0, 10.0, 100)
+        numeric_integral(gaussian, -10.0, 10.0, 1.0, 0.0000001)
     );
 }
